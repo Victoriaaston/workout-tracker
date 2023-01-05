c634ac04c25ce7e05c43ab0dd06598771da1d580
Victoriaaston <Aston.Victoria27@gmail.com>
2023-01-05 10:48:33 -0600

    index page styling

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const workoutCtrl = require("../controllers/workouts")
+const isLoggedIn = require('../config/auth');
 
 /* GET users listing. */
 router.get('/', workoutCtrl.index)
 
-router.get("/new", workoutCtrl.new)
+router.get("/new", isLoggedIn, workoutCtrl.new)
 
-router.post("/", workoutCtrl.create)
+router.post("/", isLoggedIn, workoutCtrl.create)
 
-router.get("/:day", workoutCtrl.show)
+router.get("/:day", isLoggedIn, workoutCtrl.show)
 
-router.delete("/:id", workoutCtrl.delete)
+router.delete("/:id", isLoggedIn, workoutCtrl.delete)
 
-router.get("/:id/edit", workoutCtrl.edit)
+router.get("/:id/edit", isLoggedIn, workoutCtrl.edit)
 
-router.put("/:id", workoutCtrl.update)
+router.put("/:id", isLoggedIn, workoutCtrl.update)
 
 
 module.exports = router;
